25582aaaaf4356003a84e88bd7dbcaac476c03aa
Sam Gathercole <sapagat@fastmail.com>
2021-02-03 19:12:36 +0100

    Fix linter errors

--- generate-services-csv.js
+++ generate-services-csv.js
@@ -1,13 +1,11 @@
-const fs = require('fs')
-const outputPath = process.argv[2]
-const { fetchServices } = require('./src/providers')
+const fs = require("fs");
+const outputPath = process.argv[2];
+const { fetchServices } = require("./src/providers");
 
-const allServices = fetchServices(["*"])
-let csv = 'Service Name,Service Key,Provider Name\n'
+const allServices = fetchServices(["*"]);
+let csv = "Service Name,Service Key,Provider Name\n";
 allServices.forEach((service) => {
-  csv += `${service.name},${service.key},${service.providerName}\n`
-})
+  csv += `${service.name},${service.key},${service.providerName}\n`;
+});
 
-fs.writeFileSync(outputPath, csv)
-
-
+fs.writeFileSync(outputPath, csv);
